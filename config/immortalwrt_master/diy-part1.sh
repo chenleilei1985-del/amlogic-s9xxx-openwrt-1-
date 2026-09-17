478d946e1e0c4cc0f1f3028611945aabbdcc52bc
chenleilei1985-del <chenleilei1985@gmail.com>
2026-09-17 15:11:59 +0800

    Update diy-part1.sh

--- config/immortalwrt_master/diy-part1.sh
+++ config/immortalwrt_master/diy-part1.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: DIY script (Before updating feeds — modify the default IP, hostname, theme, add/remove packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
+# DIY script (Before updating feeds)
+# Source: immortalwrt / master
 #========================================================================================================================
 
-# Add a custom feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# SSR Plus+ (fw876/helloworld)
+sed -i '1i src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 
-# Remove unnecessary packages
+# 可选：删除不需要的包
 # rm -rf package/emortal/{autosamba,ipv6-helper}
-
